03e8f3307ee18dcaee5718615e47d2016eedf3eb
Abhijit Moni <moniabhijit773@gmail.com>
2026-09-11 01:48:09 +0530

    create class employee

---
+++ create_class_emploee.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+using namespace std;
+class employee
+{
+    public:
+    string name;
+    int id;
+    int salary;
+
+
+
+    employee(string a,int b,int c){
+        name=a;
+        id=b;
+        salary=c;
+    }
+    
+
+    void display()
+    {
+        cout<<"employee name: "<<name<<endl;
+        cout<<"employee id: "<<id<<endl;
+        cout<<"salary: "<<salary<<endl;
+    }
+};
+
+    int main(){
+        employee e1("abhijit",101,60000);
+        employee e2("subhajit",102,55000);
+        e1.display();
+        e2.display();
+        return 0;
+    }
